f9d7ceaa08bcef41fc5b6fe393ba7386c1e610f2
Ruoshui Yan <ryan_yrs@hotmail.com>
2016-11-05 22:16:17 -0400

    Add apps

---
+++ Controlling Keyboard/Controlling Keyboard/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Controlling Keyboard
+//
+//  Created by Ryan Ruoshui Yan on 05/11/2016.
+//  Copyright © 2016 Ryan Ruoshui Yan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+
+
+}
+
---
+++ Multiple View Controllers/Multiple View Controllers/SecondViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SecondViewController.swift
+//  Multiple View Controllers
+//
+//  Created by Ryan Ruoshui Yan on 05/11/2016.
+//  Copyright © 2016 Ryan Ruoshui Yan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Second")
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
