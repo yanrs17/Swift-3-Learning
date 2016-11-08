b4e995ae65cb0c69499dcbe41c09949df02519b0
Ruoshui Yan <ryan_yrs@hotmail.com>
2016-11-08 01:58:42 -0500

    Finish app: Advanced Segues

--- Advanced Segues/Advanced Segues/ViewController.swift
+++ Advanced Segues/Advanced Segues/ViewController.swift
@@ -8,8 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var activeRow = 0
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // Get the row selected
+        
+//        print(indexPath.row)
+        activeRow = indexPath.row
+        performSegue(withIdentifier: "toSecondViewController", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toSecondViewController" {
+            let secondViewController = segue.destination as! SecondViewController
+            // segue.destination gets the destination of segue
+            
+            secondViewController.activeRow = activeRow
+        }
+    }
+    
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell") // identifier needs to match
+        cell.textLabel?.text = "Row \(indexPath.row)"
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
