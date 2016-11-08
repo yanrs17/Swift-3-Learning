5a23a72bf0fa139e8fe5cbdb1cfc567342a863b8
Ruoshui Yan <ryan_yrs@hotmail.com>
2016-11-07 20:35:14 -0500

    Create app: Location Aware

--- Finding User Location/Finding User Location/ViewController.swift
+++ Finding User Location/Finding User Location/ViewController.swift
@@ -29,16 +29,105 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         
         // Get current location (latitude & longitude)
         let userLocation: CLLocation = locations[0]
-        let lat = userLocation.coordinate.latitude
-        let lon = userLocation.coordinate.longitude
         
-        getLocationOfMap(lat: lat, lon: lon, delta: 0.05)
-        // Then the location of the user will be shown at the center of the map!
+//        let lat = userLocation.coordinate.latitude
+//        let lon = userLocation.coordinate.longitude
+//        getLocationOfMap(lat: lat, lon: lon, delta: 0.05)
+//        // Then the location of the user will be shown at the center of the map!
+        
+        convertCoordicateToAddress(coordination: userLocation)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func convertCoordicateToAddress(coordination: CLLocation) {
+        
+        CLGeocoder().reverseGeocodeLocation(coordination) {
+            (placemarks, error) in
+            
+            if error != nil {
+                print(error!)
+            } else {
+                if let placemark = placemarks?[0] {
+                    
+                    // 地標
+                    print(placemark)
+                    
+                    // 國家
+                    var country = placemark.country
+                    if country == nil { // To prevent crash if it is nil
+                        country = ""
+                    }
+                    
+                    // 國家代碼
+                    var isoCountryCode = placemark.isoCountryCode
+                    if isoCountryCode == nil {
+                        isoCountryCode = ""
+                    }
+                    
+                    // 時區
+                    print(placemark.timeZone!) // timeZone cannot be nil (?)
+                    
+                    // 省（州）代碼
+                    var administrativeArea = placemark.administrativeArea
+                    if administrativeArea == nil {
+                        administrativeArea = ""
+                    }
+                    
+                    // 市
+                    var subAdministrativeArea = placemark.subAdministrativeArea
+                    if subAdministrativeArea == nil {
+                        subAdministrativeArea = ""
+                    }
+                    
+                    // 縣級市（可能和市相同）
+                    var locality = placemark.locality
+                    if locality == nil {
+                        locality = ""
+                    }
+                    
+                    // 區域（如：大學）
+                    var subLocality = placemark.subLocality
+                    if subLocality == nil {
+                        subLocality = ""
+                    }
+                    
+                    // 街道
+                    var thoroughfare = placemark.thoroughfare
+                    if thoroughfare == nil {
+                        thoroughfare = ""
+                    }
+                    
+                    // 小街道
+                    var subThoroughfare = placemark.subThoroughfare
+                    if subThoroughfare == nil {
+                        subThoroughfare = ""
+                    }
+                    
+                    // 建築物名稱 Note: It is an array!
+                    var areasOfInterest = placemark.areasOfInterest?[0]
+                    if areasOfInterest == nil {
+                        areasOfInterest = ""
+                    }
+                    
+                    // 郵編
+                    var postalCode = placemark.postalCode
+                    if postalCode == nil {
+                        postalCode = ""
+                    }
+                    
+                    print(subThoroughfare! + thoroughfare!)
+                    print(subLocality!)
+                    print(subAdministrativeArea!)
+                    print(postalCode!)
+                    print(country!)
+                    
+                }
+            }
+        }
     }
     
     func getLocationOfMap(lat: CLLocationDegrees, lon: CLLocationDegrees, delta: CLLocationDegrees) {
