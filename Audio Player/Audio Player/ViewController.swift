7c11026b527bcb82f90166d11efe66d6a18e8dfa
Ruoshui Yan <ryan_yrs@hotmail.com>
2016-11-09 20:32:36 -0500

    Finish app: audio player

--- Audio Player/Audio Player/ViewController.swift
+++ Audio Player/Audio Player/ViewController.swift
@@ -7,12 +7,41 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player = AVAudioPlayer()
+    
+    @IBOutlet weak var btnPlay: UIButton!
+    @IBOutlet weak var slider: UISlider!
+    @IBAction func play(_ sender: Any) {
+        if btnPlay.titleLabel?.text == "PLAY" {
+            player.play()
+            btnPlay.setTitle("PAUSE", for: [])
+        } else {
+            player.pause()
+            btnPlay.setTitle("PLAY", for: [])
+        }
+    }
+    @IBAction func moveSlider(_ sender: Any) {
+        player.volume = slider.value
+    }
+    
+    func getAudio(audioName: String, type: String) {
+        let audioPath = Bundle.main.path(forResource: audioName, ofType: type)
+        do {
+            try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+        } catch {
+            // Process any errors
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        getAudio(audioName: "sheep", type: "mp3")
+        
     }
 
     override func didReceiveMemoryWarning() {
