c2f8ed815abfa49d9adfb4aba4db8b9e46e91549
Ruoshui Yan <ryan_yrs@hotmail.com>
2016-11-10 20:30:24 -0500

    Finish App

--- Downloading Images From Web/Downloading Images From Web/ViewController.swift
+++ Downloading Images From Web/Downloading Images From Web/ViewController.swift
@@ -10,9 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        // Download image from web
+        if let url = URL(string: "https://upload.wikimedia.org/wikipedia/commons/6/6a/Johann_Sebastian_Bach.jpg") {
+            let request = NSMutableURLRequest(url: url)
+            let task = URLSession.shared.dataTask(with: request as URLRequest) { // It is async
+                data, response, error in
+                
+                if error != nil {
+                    print(error!)
+                } else {
+                    if let data = data {
+                        if let image = UIImage(data: data) {
+                            self.imageView.image = image
+                            let documentPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+                            if documentPath.count > 0 {
+                                let documentDirectory = documentPath[0]
+                                let savePath = documentDirectory + "/image.jpg"
+                                do {
+                                    try UIImageJPEGRepresentation(image, 1)?.write(to: URL(fileURLWithPath: savePath))
+                                } catch {
+                                    print("Cannot save the image.")
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            
+            task.resume()
+        }
+        
+        // Restore image downloaded
+        let documentPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+        if documentPath.count > 0 {
+            let documentDirectory = documentPath[0]
+            let restorePath = documentDirectory + "/image.jpg"
+            imageView.image = UIImage(contentsOfFile: restorePath)
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
