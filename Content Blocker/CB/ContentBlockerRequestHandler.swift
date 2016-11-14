4fc449838309bc12078b9a9885b052031ce311aa
Ruoshui Yan <ryan_yrs@hotmail.com>
2016-11-14 05:10:15 -0500

    Create apps

---
+++ Content Blocker/CB/ContentBlockerRequestHandler.swift
@@ -0,0 +1,23 @@
+//
+//  ContentBlockerRequestHandler.swift
+//  CB
+//
+//  Created by Ryan Ruoshui Yan on 14/11/2016.
+//  Copyright © 2016 Ryan Ruoshui Yan. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ContentBlockerRequestHandler: NSObject, NSExtensionRequestHandling {
+
+    func beginRequest(with context: NSExtensionContext) {
+        let attachment = NSItemProvider(contentsOf: Bundle.main.url(forResource: "blockerList", withExtension: "json"))!
+        
+        let item = NSExtensionItem()
+        item.attachments = [attachment]
+        
+        context.completeRequest(returningItems: [item], completionHandler: nil)
+    }
+    
+}
