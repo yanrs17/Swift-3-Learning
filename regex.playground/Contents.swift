2844ba696d28422728e6456d6637c8041aed6865
Ruoshui Yan <ryan_yrs@hotmail.com>
2016-12-03 09:46:40 -0500

    Add notes: regex

---
+++ regex.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+let pattern = "\\b([a-z])\\.([a-z]{2,})@([a-z]+)\\.ac\\.uk\\b"
+let strRegex = "x.wu@strath.ac.uk, ak123@hotmail.com     e1s59@oxford.ac.uk, ee123@cooleng.co.uk, a.khan@surrey.ac.uk"
+let regex = try! NSRegularExpression(pattern: pattern, options: [])
+let matches = regex.matches(in: strRegex, options: [], range: NSRange(location: 0, length: strRegex.characters.count))
+
+//var result = [String]()
+for match in matches {
+    for n in 0..<match.numberOfRanges {
+        let range = match.rangeAt(n)
+        let r = strRegex.index(strRegex.startIndex, offsetBy: range.location) ..< strRegex.index(strRegex.startIndex, offsetBy: range.location+range.length)
+        
+//        if (regex % )
+//        result.append(strRegex.substring(with: r))
+        strRegex.substring(with: r)
+    }
+}
+
+//print(result)
+let replacedStr = regex.stringByReplacingMatches(in: strRegex, options: [], range: NSRange(location: 0, length: strRegex.characters.count), withTemplate: "($1, $2, $3)")
